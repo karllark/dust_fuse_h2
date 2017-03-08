ee6be19347adf5eba8c31a03ed44f0a0e97f6a7f
Karl Gordon <kgordon@stsci.edu>
2017-03-08 11:20:28 -0500

    Full set of data and generic plot routine working

--- get_data.py
+++ get_data.py
@@ -2,7 +2,8 @@
 
 from __future__ import (absolute_import, print_function, division)
 
-from astropy.table import Table
+import numpy as np
+from astropy.table import Table, join
 
 def get_fuse_h1_h2():
     """
@@ -15,11 +16,77 @@ def get_fuse_h1_h2():
     """
     
     data = Table.read('data/fuse_h1_h2.dat',
-                      format='ascii.commented_header')
+                      format='ascii.commented_header',
+                      header_start=-1)
+
+    # rename column to have the same name as other tables
+    data.rename_column('name','Name')
+
+    # remove the ebv column as this is superceded by another table
+    #    this ebv value is from the literature and is for each star
+    #    the correct E(B-V) value is for the calculated extinction curve
+    data.remove_column('ebv')
 
     return data
 
+def get_fuse_ext_details():
+    """
+    Read in the FUSE extinction details [A(V), R(V), etc.]
+
+    Returns
+    -------
+    data : astropy.table object
+       Table of the data [A(V), R(V), etc.]
+    """
+    
+    data = Table.read('data/fuse_ext_details.dat',
+                      format='ascii.commented_header',
+                      header_start=-1)
+
+    # create the combined uncertainties
+    keys = ['A(V)','E(B-V)','R(V)']
+    for key in keys:
+        data[key+'_unc'] = np.sqrt(np.square(data[key+'_runc']) + 
+                                   np.square(data[key+'_sunc']))
+
+    return data
+
+def get_fuse_ext_fm90():
+    """
+    Read in the FUSE extinction details [A(V), R(V), etc.]
+
+    Returns
+    -------
+    data : astropy.table object
+       Table of the data [A(V), R(V), etc.]
+    """
+    
+    data = Table.read('data/fuse_ext_fm90.dat',
+                      format='ascii.commented_header',
+                      header_start=-1)
+
+    # create the combined uncertainties
+    keys = ['CAV1','CAV2','CAV3','CAV4','x_o','gamma']
+    for key in keys:
+        data[key+'_unc'] = np.sqrt(np.square(data[key+'_runc']) + 
+                                   np.square(data[key+'_sunc']))
+
+    return data
+
+def get_merged_table():
+
+    # get the three tables to merge
+    h1h2_data = get_fuse_h1_h2()
+    ext_detail_data = get_fuse_ext_details()
+    ext_fm90_data = get_fuse_ext_fm90()
+
+    merged_table1 = join(h1h2_data, ext_detail_data, keys='Name')
+    merged_table = join(merged_table1, ext_fm90_data, keys='Name')
+
+    return(merged_table)
+
 if __name__ == '__main__':
 
-    h1h2_data = get_fuse_h1_h2()
-    print(h1h2_data.colnames)
+    merged_table = get_merged_table()
+    print(merged_table.colnames)
+    print(merged_table)
--- plot_fuse_results.py
+++ plot_fuse_results.py
@@ -2,12 +2,25 @@
 
 from __future__ import (absolute_import, print_function, division)
 
+import argparse
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
-from get_data import get_fuse_h1_h2
+from get_data import get_merged_table
 
-def plot_results(data, xparam, yparam):
+def initialize_parser():
+    '''For running from command line, initialize argparse with common args
+    '''
+    ftypes = ['png', 'jpg', 'jpeg', 'pdf', 'ps', 'eps', 'rgba',
+              'svg', 'tiff', 'tif', 'pgf', 'svgz', 'raw']
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--savefig', action='store', 
+                        default=False, choices=ftypes,
+                        help='Save figure to a file')
+    return parser
+
+def plot_results(data, xparam, yparam,
+                 fig=None, ax=None):
     """
     Plot the fuse results with specificed x and y axes
 
@@ -21,22 +34,54 @@ def plot_results(data, xparam, yparam):
 
     yparam: str
        name of column to plot as the y variable
+
+    fig : matplotlib figure object, optional
+        Figure to use for plot
+
+    ax : matplotlib axes object, optional
+        Subplot of figure to use
     """
 
     xcol = data[xparam]
-    #xcol_unc = data[xparam+'_unc']
+    if xparam+'_unc' in data.colnames:
+        xcol_unc = data[xparam+'_unc']
+    else:
+        xcol_unc = None
     ycol = data[yparam]
-    #ycol_unc = data[yparam+'unc']
+    if yparam+'_unc' in data.colnames:
+        ycol_unc = data[yparam+'_unc']
+    else:
+        ycol_unc = None
     
-    fig = plt.gcf() 
-    ax = plt.gca()
-    ax.scatter(xcol, ycol)
-
+    if fig is None:
+        fig = plt.gcf() 
+    if ax is None:
+        ax = plt.gca()
+    ax.errorbar(xcol, ycol, xerr=xcol_unc, yerr=ycol_unc, fmt='o')
+    ax.set_xlabel(xparam)
+    ax.set_ylabel(yparam)
     return fig
 
 if __name__ == '__main__':
 
-    data = get_fuse_h1_h2()
-    fig = plot_results(data, 'lognhtot', 'lognh')
+    # get the data table
+    data = get_merged_table()
+    colnames = data.colnames
+
+    parser = initialize_parser()
+    parser.add_argument('--xparam', action='store', default='A(V)', 
+                        choices=colnames, 
+                        help='Choose column type to plot')
+    parser.add_argument('--yparam', action='store', default='lognhtot', 
+                        choices=colnames, 
+                        help='Choose column type to plot')
+    args = parser.parse_args()
+
+    fig = plot_results(data, args.xparam, args.yparam)
     fig.tight_layout()
-    plt.show()
+
+    basename = 'fuse_results_' + args.xparam + '_' + args.yparam
+    if args.savefig:
+        fig.savefig('{}.{}'.format(basename, args.savefig))
+    else:
+        plt.show()
